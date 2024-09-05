f22be1a237e5fae07cebb245c116339e912a1572
Elies G. <elies.goetz@protonmail.com>
2024-09-05 09:00:29 +0200

    Get Quotes From API

---
+++ script.js
@@ -0,0 +1,23 @@
+let apiQuotes = [];
+
+// Show New Quotes
+function newQuote() {
+    // Pick a random quote from apiQuotes array
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    console.log(quote);
+}
+
+// Get Quotes From API
+async function getQuotes() {
+    const apiURL = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+    try {
+        const response = await fetch(apiURL);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getQuotes();
