6bea27f4afb6c1ef50bf207ca17bb26d66843fbc
Elies G. <elies.goetz@protonmail.com>
2024-09-05 09:44:17 +0200

    buttons work now

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote')
+
 let apiQuotes = [];
 
 // Show New Quotes
 function newQuote() {
     // Pick a random quote from apiQuotes array
     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    console.log(quote);
+    // Check if author field is blank and replace with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = "Unknown";
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check Quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+
+    quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API
@@ -19,5 +38,16 @@ async function getQuotes() {
     }
 }
 
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Even Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+s
 // On Load
 getQuotes();
